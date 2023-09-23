927c46da0647fbc97ca05f49112ab340dd8e0c05
Bonnie <hsuu816@gmail.com>
2023-09-23 14:25:20 +0800

    feat:print result

--- drama_comment.py
+++ drama_comment.py
@@ -6,16 +6,17 @@ comment_collection = mongo_connect_comment.get_collection()
 mongo_connect_drama = MongoDBConnector('watchnext', 'drama')
 drama_collection = mongo_connect_drama.get_collection()
 
+def comment_to_drama():
+    drama_data = drama_collection.find({})
+    drama_list = []
+    for drama in drama_data:
+        drama_list.append(drama["name"])
 
-drama_data = drama_collection.find({})
-drama_list = []
-for drama in drama_data:
-    drama_list.append(drama["name"])
+    comment_data = comment_collection.find({})
 
-comment_data = comment_collection.find({})
-
-for comment in comment_data:
-    title = comment["title"]
-    for drama in drama_list:
-        if drama in title:
-            comment_collection.update_one({"_id": comment["_id"]}, {"$set": {"drama_name": drama}})
+    for comment in comment_data:
+        title = comment["title"]
+        for drama in drama_list:
+            if drama in title:
+                result = comment_collection.update_one({"_id": comment["_id"]}, {"$set": {"drama_name": drama}})
+                print(result)
