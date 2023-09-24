bfe7225233c9cf57a84d0f0a7f7a49dd15dca24c
Bonnie <hsuu816@gmail.com>
2023-09-24 17:54:01 +0800

    feat: add detail api, modify home page api and category api

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, jsonify
-from mongodb import MongoDBConnector
 import urllib.parse
+
+from models.mongodb import MongoDBConnector
+from models.mongo_aggregate import *
 
 app = Flask(__name__)
 
@@ -8,59 +10,12 @@ app = Flask(__name__)
 mongo_connect = MongoDBConnector('watchnext', 'drama')
 drama_collection = mongo_connect.get_collection()
 
-drama_comment = [
-    {
-        "$lookup": {
-            "from": "comment",
-            "localField": "name",
-            "foreignField": "drama_name",
-            "as": "comments",
-            "pipeline": [
-                {
-                    "$project": {
-                        "_id": 0,
-                        "drama_name": 0
-                    }
-                }
-            ]
-        }
-    },
-    {
-        "$limit": 20  # 限制數量
-    }
-]
-
-hot_drama = [
-    {
-        "$lookup": {
-            "from": "comment",
-            "localField": "name",
-            "foreignField": "drama_name",
-            "as": "comments"
-        }
-    },
-    {
-        "$project": {
-            "_id": 0,
-            "name": 1,
-            "image":1,
-            "comment_count": { "$size": "$comments" } # 計算評論數
-        }
-    },
-    {
-        "$sort": { "comment_count": -1 } # 依照評論多到少排序
-    },
-    {
-        "$limit": 10  # 只顯示前十名
-    }
-]
-
 
 @app.route('/', methods=['GET'])
 def get_drama():
     # 從mongodb中擷取資料
-    drama_data = drama_collection.aggregate(drama_comment)
-    hot_drama_data = drama_collection.aggregate(hot_drama)
+    drama_data = drama_collection.find({}).limit(20)
+    hot_drama_data = drama_collection.aggregate(hot_drama())
     return render_template('index.html', dramas=drama_data, hot_drama=hot_drama_data)
 
 @app.route('/api/v1/category/<category>', methods=['GET'])
@@ -68,6 +23,14 @@ def category_select(category):
     encoded_category = urllib.parse.unquote(category)
     drama_data = list(drama_collection.find({"categories": encoded_category}, {"_id": 0}).limit(20))
     return jsonify({"dramas": drama_data})
+
+
+@app.route('/api/v1/detail/<name>',  methods=['GET'])
+def get_drama_detail(name):
+    encoded_name = urllib.parse.unquote(name)
+    drama_detail_data = list(drama_collection.aggregate(drama_detail(encoded_name)))
+    return render_template('detail.html', dramas=drama_detail_data)
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')
