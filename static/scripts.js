8d1c5302a3c65da2a26ae15ca92deacc6c6791d1
Bonnie <hsuu816@gmail.com>
2023-09-24 15:33:47 +0800

    feat: add scripts.js

---
+++ static/scripts.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var buttons = document.querySelectorAll("button[data-category]");
+    
+    buttons.forEach(function(button) {
+        button.addEventListener("click", function() {
+            var category = encodeURIComponent(this.getAttribute("data-category"));
+            console.log("Button clicked!");
+            
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", "/api/v1/category/" + encodeURIComponent(category), true);
+            
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState === 4 && xhr.status === 200) {
+                    var responseData = JSON.parse(xhr.responseText);
+                    // 將原本顯示戲劇資訊的class內容清空
+                    var dramaContainer = document.querySelector(".drama-container");
+                    dramaContainer.innerHTML = "";
+
+                    // 將 responseData 中的資料插入容器中
+                    responseData.dramas.forEach(function(drama) {
+                        var dramaElement = document.createElement("div");
+                        dramaElement.className = "drama";
+
+                        // 根據 responseData 生成 HTML
+                        dramaElement.innerHTML = `
+                            <div class="detail">
+                                <img src="${drama.image}" alt="${drama.name}">
+                                <h3>${drama.name}</h3>
+                                <p>${drama.eng_name}</p>
+                                <p>類別：${drama.categories.join(", ")}</p>
+                            </div>
+                        `;
+
+                        // 插入戲劇資訊
+                        dramaContainer.appendChild(dramaElement);
+                    });
+                }
+            };
+            
+            xhr.send();
+        });
+    });
+});
