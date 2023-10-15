9dc23f831e147e76716f28c3a9a0cbf9ecc37bf4
Bonnie <hsuu816@gmail.com>
2023-10-15 17:55:02 +0800

    style: modify mongo_aggregate indentation

--- app/server/models/mongo_aggregate.py
+++ app/server/models/mongo_aggregate.py
@@ -19,76 +19,41 @@ def drama_detail(id):
 
 def hot_drama(limit):
     hot_drama = [
-        {
-            "$group": {
-                "_id": "$drama_name",
-                "count": {"$sum": 1}
-            }
-        },
-        {
-            "$sort": { "count": -1 }
-        },
-        {
-            "$skip":1
-        },
-        {
-            "$limit": limit
-        },
-        {
-            "$lookup": {
-                "from": "drama",
-                "localField": "_id",
-                "foreignField": "name",
-                "as": "detail"
-            }
-        },
-        {
-            "$project": {
-              "_id": 0,
-              "count": 1,
-              "detail": {
-                "_id": 1,
-                "name": 1,
-                "image": 1,
-                "categories": 1
-                }
-            }
-        }
+        {"$group": {"_id": "$drama_name", "count": {"$sum": 1}}},
+        {"$sort": { "count": -1 }},
+        {"$skip":1},
+        {"$limit": limit},
+        {"$lookup": {
+            "from": "drama",
+            "localField": "_id",
+            "foreignField": "name",
+            "as": "detail"
+        }},
+        {"$project": {
+            "_id": 0,
+            "count": 1,
+            "detail": {"_id": 1, "name": 1, "image": 1, "categories": 1}
+        }}
     ]
     return hot_drama
 
 def recommend_same_category_drama(category, name):
     recommend_same_category_drama = [
-        {
-            "$match": {
-            "categories": category,
-            "name": { "$ne": name }
-            }
-        },
-        {
-            "$limit": 50
-        },
-        {
-            "$lookup": {
+        {"$match": {"categories": category, "name": { "$ne": name }}},
+        {"$limit": 50},
+        {"$lookup": {
             "from": "comment",
             "localField": "name",
             "foreignField": "drama_name",
             "as": "comment"
-            }
-        },
-        {
-            "$project": {
+        }},
+        {"$project": {
             "name": 1,
             "image": 1,
             "comment_count": { "$size": "$comment" }
-            }
-        },
-        {
-            "$sort": {"comment_count": -1}
-        },
-        {
-            "$limit": 4
-        }
+        }},
+        {"$sort": {"comment_count": -1}},
+        {"$limit": 4}
 ]
     return recommend_same_category_drama
 
@@ -102,8 +67,7 @@ def content_based_rec_drama(id):
             "localField": "drama2_id",
             "foreignField": "_id",
             "as": "detail"
-            }
-        },
+        }},
         {"$project": {
             "_id": 0,
             "detail": {
@@ -111,9 +75,7 @@ def content_based_rec_drama(id):
                 "name": 1,
                 "image": 1,
                 "categories": 1
-                }
-            }
-        }
+        }}}
     ]
     return content_based_rec_drama
 
@@ -125,18 +87,16 @@ def user_rating_drama(user_id):
             "localField": "drama_id",
             "foreignField": "_id",
             "as": "drama_data"
-            }
-        },
+        }},
         {"$project": {
             "user_id":1,
             "drama_id": 1,
             "rating": 1,
             "create_time": 1,
             "drama_data":{"name":1,"image":1}
-            }
-        },
+        }},
         {"$sort": {"create_time": -1}}
-        ]
+    ]
     return user_rating_drama
 
 def similarity_user_like(user_id, already_rating_drama):
@@ -145,22 +105,19 @@ def similarity_user_like(user_id, already_rating_drama):
             "user_id": user_id, 
             "rating": {"$in": [4, 5]},
             "drama_id": {"$nin": already_rating_drama}
-            }
-        },
+        }},
         {"$lookup": {
             "from": "drama",
             "localField": "drama_id",
             "foreignField": "_id",
             "as": "drama_data"
-            }
-        },
+        }},
         {"$project": {
             "rating": 1,
             "drama_data": {"_id":1, "name": 1, "image": 1, "categories": 1}
-            }
-        },
+        }},
         {"$limit": 20}
-        ]
+    ]
     return similarity_user_like
 
 def member_content_based_rec_drama(id_list, limit):
